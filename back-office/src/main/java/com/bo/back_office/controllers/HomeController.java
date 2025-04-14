2cf4d85b3aaf2a57febcfb08d2db0ab2b816e35d
Eddie Hsu <aoe102198@gmail.com>
2025-04-14 23:59:33 +0800

    Update homeController routing path

--- back-office/src/main/java/com/bo/back_office/controllers/HomeController.java
+++ back-office/src/main/java/com/bo/back_office/controllers/HomeController.java
@@ -1,11 +1,14 @@
 package com.bo.back_office.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
-
-    @GetMapping("/")
-    public String index() {
-        return "index"; // 對應 resources/templates/index.html
+    @GetMapping("/home")
+    public String index(Model model) {
+        model.addAttribute("pageTitle", "首頁");
+        return "index";
     }
 }
